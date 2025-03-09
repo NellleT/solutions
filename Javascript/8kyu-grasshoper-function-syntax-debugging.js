f97c226441489f6336c9c913efb30413a49bb5c7
Влад Захаров <vladzaharov4015@gmail.com>
2025-03-09 22:31:52 +0100

    feat: solve 8kyu task (JS)

---
+++ Javascript/8kyu-grasshoper-function-syntax-debugging.js
@@ -0,0 +1,6 @@
+// Codewars: Grasshopper - Function syntax debugging
+// 8 kyu: https://www.codewars.com/kata/56dae9dc54c0acd29d00109a
+
+function main (verb, noun) {
+    return `${verb}${noun}`
+  }
