1a15309e3353d4ed7422de7f92f5d236a1b2c9bf
Влад Захаров <vladzaharov4015@gmail.com>
2025-03-26 23:40:16 +0100

    feat: solve 8kyu simple fundamentals  task (JS)

---
+++ Javascript/8kyu-fundamentals-return.js
@@ -0,0 +1,26 @@
+// Codewars: Fundamentals: Return
+// 8 kyu: https://www.codewars.com/kata/55a5befdf16499bffb00007b
+
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a ** b
+}
+    
+function subt(a,b){
+    return a - b
+}
