e0e848d75dead52ceb124bbce934250007522ca1
Влад Захаров <vladzaharov4015@gmail.com>
2025-05-24 22:30:00 +0200

    feat: solve 7kyu build a square task (JS)

---
+++ codewars-solutions/Javascript/7kyu-build-a-square.js
@@ -0,0 +1,6 @@
+// Codewars: Build a square
+// 7 kyu: https://www.codewars.com/kata/59a96d71dbe3b06c0200009c
+
+function generateShape(integer){
+  return Array(integer).fill("+".repeat(integer)).join("\n")
+}
