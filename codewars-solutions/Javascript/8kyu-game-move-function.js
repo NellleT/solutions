15067c0e03ff6aa2c86be0b762bec609ea92fed7
Влад Захаров <vladzaharov4015@gmail.com>
2025-05-29 23:28:10 +0200

    feat: solve simple 8kyu game task (JS)

---
+++ codewars-solutions/Javascript/8kyu-game-move-function.js
@@ -0,0 +1,6 @@
+// Codewars: Grasshopper - Terminal game move function
+// 8 kyu: https://www.codewars.com/kata/563a631f7cbbc236cf0000c2
+
+function move (position, roll) {
+  return roll * 2 + position
+}
