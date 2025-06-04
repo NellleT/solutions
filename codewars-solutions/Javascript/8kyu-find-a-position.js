9accb4836a8199a2ed2b71b04861527c3583c92c
Влад Захаров <vladzaharov4015@gmail.com>
2025-06-04 23:56:19 +0200

    solve one more simple 8kyu alphabet letter task (JS)

---
+++ codewars-solutions/Javascript/8kyu-find-a-position.js
@@ -0,0 +1,6 @@
+// Codewars: Find the position!
+// 8 kyu: https://www.codewars.com/kata/5808e2006b65bff35500008f
+
+function position(letter) {
+  return `Position of alphabet: ${"abcdefghijklmnopqrstuvwxyz".indexOf(letter) + 1}`;
+}
