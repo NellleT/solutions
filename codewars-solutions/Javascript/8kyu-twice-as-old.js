d15c529b5b43d9d6fa7078892da68815ec3c07e0
Влад Захаров <vladzaharov4015@gmail.com>
2025-07-10 21:42:14 +0200

    feat: solve simple 8kyu maths kata (JS)

---
+++ codewars-solutions/Javascript/8kyu-twice-as-old.js
@@ -0,0 +1,6 @@
+// Codewars: Twice as old
+// 8 kyu: https://www.codewars.com/kata/5b853229cfde412a470000d0
+
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  return Math.abs(dadYearsOld - sonYearsOld * 2)
+}
