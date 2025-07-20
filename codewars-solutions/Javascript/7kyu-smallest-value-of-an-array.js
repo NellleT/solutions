3aa45a0c519cbb3c03d2fa9293ec21bb1d5acb7c
Влад Захаров <vladzaharov4015@gmail.com>
2025-07-21 00:57:08 +0200

    feat: solve 7kyu value / index of a smallest element in array kata (JS)

---
+++ codewars-solutions/Javascript/7kyu-smallest-value-of-an-array.js
@@ -0,0 +1,6 @@
+// Codewars: Smallest value of an array
+// 7 kyu: https://www.codewars.com/kata/544a54fd18b8e06d240005c0
+
+function min(arr, toReturn) { 
+  return toReturn === 'value' ? Math.min(...arr) : arr.indexOf(Math.min(...arr));
+}
