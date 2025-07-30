e39dec5b743a91545d06fa24346b8d771a81ce8f
Влад Захаров <vladzaharov4015@gmail.com>
2025-07-30 23:40:54 +0200

    feat: solve mathematical 8kyu distance between pillars kata (JS)

---
+++ codewars-solutions/Javascript/8kyu-pillars.js
@@ -0,0 +1,6 @@
+// Codewars: Pillars
+// 8 kyu: https://www.codewars.com/kata/5bb0c58f484fcd170700063d
+
+function pillars(numPill, dist, width) {
+  return numPill >= 2 ? (numPill - 1) * dist * 100 + (numPill - 2) * width : 0;
+}
