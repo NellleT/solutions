ded40a269676330f566ffe4f495e8a8aad9de847
Влад Захаров <vladzaharov4015@gmail.com>
2025-08-09 23:57:58 +0200

    feat: solve international greetings 8kyu kata (JS)

---
+++ codewars-solutions/Javascript/8kyu-did-she-say-hallo.js
@@ -0,0 +1,7 @@
+// Codewars: Did she say hallo?
+// 8 kyu: https://www.codewars.com/kata/56a4addbfd4a55694100001f
+
+
+function validateHello(greetings) {
+  return /hello|ciao|salut|hallo|hola|ahoj|czesc/i.test(greetings);
+}
