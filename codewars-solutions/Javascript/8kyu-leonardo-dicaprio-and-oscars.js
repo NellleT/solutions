acf1d57dd6bae113c4096368235e0b54bc01f2f1
Влад Захаров <vladzaharov4015@gmail.com>
2025-09-06 23:27:22 +0200

    feat: solve simple 8kyu Dicaprio and Oscars kata (JS)

---
+++ codewars-solutions/Javascript/8kyu-leonardo-dicaprio-and-oscars.js
@@ -0,0 +1,9 @@
+// Codewars: Leonardo Dicaprio and Oscars
+// 8 kyu: https://www.codewars.com/kata/56d49587df52101de70011e4
+
+function leo(oscar) {
+  if (oscar === 88) return "Leo finally won the oscar! Leo is happy";
+  else if (oscar === 86) return "Not even for Wolf of wallstreet?!";
+  else if (oscar !== 88 && oscar !== 86 && oscar < 88) return "When will you give Leo an Oscar?";
+  else return "Leo got one already!";
+}
