24930f313c4386f938a13952e4f15081a8f9e43b
Влад Захаров <vladzaharov4015@gmail.com>
2025-10-07 23:13:54 +0200

    feat: solve simple 8kyu grader kata (JS)

---
+++ codewars-solutions/Javascript/8 Kyu/8kyu-grader.js
@@ -0,0 +1,10 @@
+// Codewars: Grader
+// 8 kyu: https://www.codewars.com/kata/53d16bd82578b1fb5b00128c
+
+function grader(score) {
+  if (score > 1 || score < 0.6) return "F";
+  else if (score >= 0.9) return "A";
+  else if (score >= 0.8) return "B";
+  else if (score >= 0.7) return "C";
+  else if (score >= 0.6) return "D";
+}
